293b4639d151b8644b7122ec460e19251987e4fd
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-24 22:44:45 +0530

    index file

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const DB = require('./models/connDB')
+DB()
+const port = 3001
+
+app.listen(port,()=>console.log(`Server is running at ${port}`))
