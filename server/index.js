ca4ecf19fa827aa179a7b788f233b025003011ca
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-26 19:55:52 +0530

    .env config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const DB = require('./models/connDB')
 DB()
 const port = 3001
+
+app.use(express.json())
 
 app.listen(port,()=>console.log(`Server is running at ${port}`))
