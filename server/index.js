546712e8204a06a278eb23178c9fffb764a08ebe
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-26 20:34:36 +0530

    routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const routes = require("./routes/userRoutes")
 const DB = require('./models/connDB')
 DB()
 const port = 3001
 
 app.use(express.json())
-
+app.use("/api",routes)
 app.listen(port,()=>console.log(`Server is running at ${port}`))
