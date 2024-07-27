d039c8487ba9ed742e390f5ef982a350bc85a111
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-27 21:35:20 +0530

    signin route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const {signup} = require('../controller/authController')
+const {signup, signin} = require('../controller/authController')
 const express = require('express')
 const router = express.Router()
 
 router.post('/signup',signup)
+router.get('/signin', signin)
 
 module.exports = router
