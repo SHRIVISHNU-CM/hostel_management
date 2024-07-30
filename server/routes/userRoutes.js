991e8b91d632d9c7cd5bf33c8b23f4db44fc43d3
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-30 21:56:49 +0530

    all details, details

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
-const {signup, signin} = require('../controller/authController')
+const {signup, signin,userDetails,AllDetails} = require('../controller/authController')
 const express = require('express')
 const router = express.Router()
 
 router.post('/signup',signup)
 router.get('/signin', signin)
+router.get("/all",AllDetails)
+router.post('/details',userDetails)
 
 module.exports = router
