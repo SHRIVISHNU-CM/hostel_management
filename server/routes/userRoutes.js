4931b81bb1630f864a255961b077028603ac6f4d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-31 12:34:33 +0530

    delete route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const {signup, signin,userDetails,AllDetails} = require('../controller/authController')
+const {signup, signin,userDetails,AllDetails,userdelete} = require('../controller/authController')
 const express = require('express')
 const router = express.Router()
 
@@ -6,5 +6,6 @@ router.post('/signup',signup)
 router.get('/signin', signin)
 router.get("/all",AllDetails)
 router.post('/details',userDetails)
+router.delete('/delete/:id',userdelete)
 
 module.exports = router
