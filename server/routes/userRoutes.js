e23d5a6327455ffa892a759c6eb4b3c856ef26de
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-01 19:42:42 +0530

    post

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/signup',signup)
-router.get('/signin', signin)
+router.post('/signin', signin)
 router.get("/all",AllDetails)
 router.post('/details',userDetails)
 router.delete('/delete/:id',userdelete)
