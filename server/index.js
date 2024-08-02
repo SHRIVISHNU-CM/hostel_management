bbba3d618c9a5f4e66f7713b78a5deb2e3d3ac9e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-02 20:26:46 +0530

    cloudinary config

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const app = express()
 const routes = require("./routes/userRoutes")
 const adminRoutes = require("./routes/adminRoutes")
 const DB = require('./models/connDB')
+const cloudinary = require("cloudinary").v2
 DB()
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 const port = 3001
 
+
+
 app.use(express.json())
-app.use("/api",routes)
-app.use("/admin",adminRoutes)
-app.listen(port,()=>console.log(`Server is running at ${port}`))
+app.use("/api", routes)
+app.use("/admin", adminRoutes)
+app.listen(port, () => console.log(`Server is running at ${port}`))
