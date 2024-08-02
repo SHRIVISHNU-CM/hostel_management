5f4492af0c50c093de25387296fb610d3cf5f326
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-02 19:43:16 +0530

    admin routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require("./routes/userRoutes")
+const adminRoutes = require("./routes/adminRoutes")
 const DB = require('./models/connDB')
 DB()
 const port = 3001
 
 app.use(express.json())
 app.use("/api",routes)
+app.use("/admin",adminRoutes)
 app.listen(port,()=>console.log(`Server is running at ${port}`))
