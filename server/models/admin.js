6a46bafd2bd418af349a62a9f908a2203c1aa131
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-03 20:01:24 +0530

    admin schema

---
+++ server/models/admin.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const { Schema } = mongoose
+
+const admin = new Schema({
+    name: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    adminPic: [{
+        type: "ObjectId",
+        ref: "adminDB"
+    }]
+})
+module.exports = mongoose.model("admin", admin)
