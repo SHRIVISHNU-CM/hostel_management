e680b69f049c5bfa69abbb7a942ef9b69ffafb9a
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-04 19:32:41 +0530

    added secret key for admin users

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -3,26 +3,33 @@ const adminDB = require("../models/adminDB")
 const cloudinary = require("cloudinary").v2
 const signup = async (req, res) => {
     try {
-        const { name, password } = req.body
-        if (!name || !password) {
+        const { name, password, secret } = req.body
+        if (!name || !password || !secret) {
             return res.status(401).json({
-                "message": "Please provide proper name & passord"
+                "message": "Please provide proper name, password, and Secret"
             })
         }
-        const username = await admin.findOne({ name: name })
+        if (secret === "hi") {
+            const username = await admin.findOne({ name: name })
 
-        if (username) {
-            return res.status(400).json({
-                "message": "Username already exist"
+            if (username) {
+                return res.status(400).json({
+                    "message": "Username already exist"
+                })
+            }
+            const response = await admin.create({
+                name, password
+            })
+            console.log(response)
+            return res.status(200).json({
+                "message": "Successfully admin created"
+            })
+
+        }else{
+            return res.status(401).json({
+                "message":"Admin Unauthorized"
             })
         }
-        const response = await admin.create({
-            name, password
-        })
-        console.log(response)
-        return res.status(200).json({
-            "message": "Successfully admin created"
-        })
 
     } catch (error) {
         console.log(error.message)
@@ -61,28 +68,28 @@ const signin = async (req, res) => {
 }
 
 //post rooms
-const DbRooms = async (req,res) =>{
+const DbRooms = async (req, res) => {
     try {
-        
+
         const roomsInfo = req.body
-        if(req.file){
+        if (req.file) {
             const result = await cloudinary.uploader.upload(req.file.path)
             roomsInfo.cloudinary_uri = result.secure_url
         }
         const newRoom = new adminDB(roomsInfo)
         const response = await admin.findById(roomsInfo.main)
 
-        
+
 
         if (!response) {
             return res.status(400).json({
-                "message":"User not found"
+                "message": "User not found"
             })
         }
         const info = await newRoom.save()
         console.log(info)
 
-        if (! response.adminPic){
+        if (!response.adminPic) {
             admin.adminPic = []
         }
         response.adminPic.push(info._id)
@@ -91,7 +98,7 @@ const DbRooms = async (req,res) =>{
     } catch (error) {
         console.log(error.message)
         return res.status(400).json({
-            "message":error.message
+            "message": error.message
         })
     }
 }
@@ -99,7 +106,7 @@ const DbRooms = async (req,res) =>{
 //all rooms
 
 
-const AllRooms = async(req,res) => {
+const AllRooms = async (req, res) => {
     try {
         const response = await admin.find().populate("adminPic")
         console.log(response)
@@ -107,67 +114,67 @@ const AllRooms = async(req,res) => {
     } catch (error) {
         console.log(error)
         return res.status(400).json({
-            "message":error.message
+            "message": error.message
         })
     }
 }
 
 //update 
 
-const UpdateRoom = async(req,res) =>{
+const UpdateRoom = async (req, res) => {
     try {
         const updateInfo = req.body
-        const RoomsId = await adminDB.findById({_id:req.params.id})
+        const RoomsId = await adminDB.findById({ _id: req.params.id })
         const id = RoomsId.secure_url
-        
-        if(!RoomsId){
+
+        if (!RoomsId) {
             return res.status(400).json({
-                "message":"Enter Correct ID"
+                "message": "Enter Correct ID"
             })
         }
-        if(req.file){
-            if (id){
+        if (req.file) {
+            if (id) {
                 await cloudinary.uploader.destroy(id)
             }
         }
         const PicUpload = await cloudinary.uploader.upload(req.file.path)
         updateInfo.cloudinary_uri = PicUpload.secure_url
         console.log(PicUpload.secure_url)
-        const updatedInfo = await adminDB.findByIdAndUpdate({_id:req.params.id},{$set:updateInfo},{new:true})
+        const updatedInfo = await adminDB.findByIdAndUpdate({ _id: req.params.id }, { $set: updateInfo }, { new: true })
         console.log(updatedInfo)
         return res.status(200).json({
-            "message":updatedInfo
+            "message": updatedInfo
         })
 
-        
+
     } catch (error) {
         console.log(error.message)
         return res.status(400).json({
-            "message":error.message
+            "message": error.message
         })
     }
 }
 
 //delete roomsDB
 
-const DeleteRoomDB = async(req,res)=>{
+const DeleteRoomDB = async (req, res) => {
     try {
-        const RoomDB = await adminDB.findByIdAndDelete(req.params.id) 
-        const response = await admin.updateOne({adminPic:req.params.id},{$pull:{adminPic:req.params.id}})
+        const RoomDB = await adminDB.findByIdAndDelete(req.params.id)
+        const response = await admin.updateOne({ adminPic: req.params.id }, { $pull: { adminPic: req.params.id } })
 
         console.log(response)
         return res.status(200).json({
-            "message":response
+            "message": response
         })
     } catch (error) {
         console.log(error)
         return res.status(400).json({
-            "message":error.message
+            "message": error.message
         })
     }
 }
 
- module.exports = {
+module.exports = {
     signup,
     signin,
     DbRooms,
