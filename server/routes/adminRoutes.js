c2685351a154acc1db94b9bf6a2a793d4ad286b8
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-04 12:58:54 +0530

    route for delete adminDB

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,4 +1,4 @@
-const {signup,signin,DbRooms,AllRooms,UpdateRoom} = require('../controller/adminController')
+const {signup,signin,DbRooms,AllRooms,UpdateRoom,DeleteRoomDB} = require('../controller/adminController')
 const express = require("express")
 const router = express.Router()
 const multer = require("../multer")
@@ -8,5 +8,6 @@ router.post("/signin",signin)
 router.post("/rooms",multer.single("image"),DbRooms)
 router.get("/allRooms",AllRooms)
 router.put('/roomsUpdate/:id',multer.single("image"),UpdateRoom)
+router.delete("/deleteDB/:id",DeleteRoomDB)
 
 module.exports = router
