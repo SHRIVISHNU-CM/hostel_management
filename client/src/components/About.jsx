4c7aad970faa14bf92b9866474af82e72d3665d7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-06 19:55:56 +0530

    template

---
+++ client/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <p className='text-2xl font-bold'>About Page</p>
+            
+        </>
+    )
+}
+
+export default About
---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <>
+        <p className='text-xl font-extralight'>Contact Page</p>
+    </>
+  )
+}
+
+export default Contact
