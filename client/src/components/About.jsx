06f7cc1d6297942743bde283623698ab661a72a1
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-06 21:06:26 +0530

    about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 
 function About() {
     return (
         <>
-            <p className='text-2xl font-bold'>About Page</p>
-            
+            <p className='text-2xl font-bold text-center text-orange-600'>About Page</p>
+            <p className='font-light px-2'>
+                A hostel is a form of low-cost, short-term shared sociable lodging where guests can rent a bed, usually a bunk bed in a dormitory sleeping 4–20 people,with shared use of a lounge and usually a kitchen. Rooms can be mixed or single-sex and have private or shared bathrooms. Private rooms may also be available. Hostels are popular forms of lodging for backpackers,however very few impose age limits, so hostels are an option for travellers of all ages and styles. The benefits of hostels include lower costs and opportunities to meet people from different places, find travel partners, and share travel experiences. Some hostels, such as in India or Hostelling International, cater to a niche market of travelers. Different hostels can be known for offering different experiences. For example, one hostel might feature in-house social gatherings such as movie nights or communal dinners, another might feature local tours, one might be known for its parties, and another might have a quieter place to relax in serenity, or be located on the beach. Newer hostels focus on a more trendy design, some of which are on par with boutique hotels. Some may cater to older digital nomads, global nomads, and perpetual travelers who prefer slightly more upmarket private rooms or a quieter atmosphere. Hostels may also differentiate themselves by being environmentally friendly ecohostels. In countries where wages are lower, the cost of staying at a hostel may be similar to staying in a budget hotel.
+            </p>
+
         </>
     )
 }
