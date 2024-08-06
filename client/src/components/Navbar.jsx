8d3b17c4da7ee202996012def8888553546a0f3e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-06 19:55:15 +0530

    navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { FaBarsStaggered } from "react-icons/fa6";
+import { NavLink, Link } from "react-router-dom"
+import logo from "../assets/logo.png"
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar bg-base-200 ">
+                <div className="block lg:hidden navbar-start">
+                    <div className="dropdown">
+                        <div className="btn btn-ghost " tabIndex={0} role="button">
+                            <FaBarsStaggered />
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-200 rounded-box z-10 mt-2 w-52 shadow"
+                        >
+                            <li>About</li>
+                            <li>Contact Us</li>
+
+                        </ul>
+
+                    </div>
+
+
+                </div>
+                <div className="navbar-start lg:navbar-center  ">
+                    <div className="flex items-center btn hover:btn-ghost">
+                        <img src={logo} className="w-[20px]" />
+                        <Link to="/" className="font-semibold font-serif">kailasa</Link>
+                    </div>
+
+                </div>
+                <div className="hidden lg:block lg:navbar-end">
+                    <div className="flex justify-around">
+                        <NavLink to='/about'
+                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>About</NavLink>
+                        <NavLink to="/contact"
+                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Contact Us</NavLink>
+                        <NavLink to="/register"
+                         className={({isActive})=> `${isActive? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"} `}>Rooms</NavLink>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
