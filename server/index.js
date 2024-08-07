fb3d7457c757b636b036075117c0b21d21519748
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-07 16:08:46 +0530

    localhost

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const port = 3001
 app.use(express.json())
 app.use(cors(
     {
-        origin:"http://localhost:5173/",
+        origin:"http://localhost:5173",
         credentials:true
     }
 ))
