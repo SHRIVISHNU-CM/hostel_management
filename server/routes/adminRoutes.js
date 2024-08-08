51815e8b77b8a6606c0bf804a16ee2321315ab0f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-08 18:45:39 +0530

    contact routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,4 +1,4 @@
-const {signup,signin,DbRooms,AllRooms,UpdateRoom,DeleteRoomDB} = require('../controller/adminController')
+const {signup,signin,DbRooms,AllRooms,UpdateRoom,DeleteRoomDB,contactus,allContact} = require('../controller/adminController')
 const express = require("express")
 const router = express.Router()
 const multer = require("../multer")
@@ -9,5 +9,7 @@ router.post("/rooms",multer.single("image"),DbRooms)
 router.get("/allRooms",AllRooms)
 router.put('/roomsUpdate/:id',multer.single("image"),UpdateRoom)
 router.delete("/deleteDB/:id",DeleteRoomDB)
+router.post("/contactus",contactus)
+router.get("/allContactus",allContact)
 
 module.exports = router
