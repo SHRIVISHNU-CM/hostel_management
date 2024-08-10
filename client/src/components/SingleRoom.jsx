15100cec4e6bcbfdfeb1fecd863d0584003b2d4d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-10 20:16:25 +0530

    signal initial template

--- client/src/components/SingleRoom.jsx
+++ client/src/components/SingleRoom.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react'
-import { useParams} from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
 import axios from "axios"
 function SingleRoom() {
-    const{id} = useParams()
-    useEffect(()=>{
-        axios.get(`http://localhost:3001/admin/room/${id}`)
-        .then((res)=>{
-          console.log(res.data.message[0].sharing)
-        })
-    
-    })
+  const [room, SetRoom] = useState([])
+  const { id } = useParams()
+  useEffect(() => {
+    axios.get(`http://localhost:3001/admin/room/${id}`)
+      .then((res) => {
+        console.log(res.data.message[0].sharing)
+        SetRoom(res.data.message[0])
+      })
+
+  }, [])
   return (
     <>
+      <div className='card w-[600px] bg-base-200 shadow-xl'>
+        <figure className='p-2'>
+          <img src={room.cloudinary_uri} />
+        </figure>
+        <div className='card-body'>
 
+          <div>
+            <button className='btn btn-active'>Book Now</button>
+          </div>
+        </div>
+
+      </div>
+      <p>{room.sharing}</p>
     </>
   )
 }
