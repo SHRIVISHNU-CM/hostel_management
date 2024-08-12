b8635d440efc77a035f46a76d4cb2411a9ec0a99
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-12 20:58:29 +0530

    loader

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='flex justify-center'>
+      <p className=' text-4xl font-semibold text-green-900'>Loading .... </p>
+    </div>
+  )
+}
+
+export default Loading
