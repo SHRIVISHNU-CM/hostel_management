24e79a1c3fc61e31d3b240807c0c2940d446f542
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-12 11:18:07 +0530

    oneuser

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
-const {signup, signin,userDetails,AllDetails,userdelete,deleteDetails,updateDetails,profileUpdate} = require('../controller/authController')
+const {signup, signin,userDetails,AllDetails,userdelete,deleteDetails,updateDetails,profileUpdate,OneUser} = require('../controller/authController')
 const express = require('express')
 const router = express.Router()
 
 router.post('/signup',signup)
 router.post('/signin', signin)
 router.get("/all",AllDetails)
+router.get("/oneuser/:id",OneUser)
 router.post('/details',userDetails)
 router.delete('/delete/:id',userdelete)
 router.delete("/delete/details/:id",deleteDetails)
