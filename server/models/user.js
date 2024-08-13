8c1e3c28587b43919ade62f3c2e26d7a59b69678
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-13 21:00:26 +0530

    changed model

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,33 @@
 const mongoose = require("mongoose");
 
-const {Schema } = mongoose
+const { Schema } = mongoose
 
 const User = new Schema({
-    name:{
-        type:String,
-        required:[true,"Enter username"]
+    name: {
+        type: String,
+        required: [true, "Enter username"]
     },
-    password:{
-        type:String,
-        required:[true,"Please enter Password"]
+    password: {
+        type: String,
+        required: [true, "Please enter Password"]
     },
-    UserType:{
-        type:String,
-        default:"student",
-        enum:["student","admin"]
+    phone: {
+        type: Number
     },
-    rooms:[{
-        type:"ObjectId",
-        ref:"userDetails"
+    address: {
+        type: String
+    },
+    college: {
+        type: String
+    },
+    UserType: {
+        type: String,
+        default: "student",
+        enum: ["student", "other"]
+    },
+    rooms: [{
+        type: "ObjectId",
+        ref: "userDetails"
     }]
-},{timestamps:true})
+}, { timestamps: true })
 module.exports = mongoose.model("UserProfile", User)
