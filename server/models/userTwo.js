ad65e9750af801934b49bf5c04238084972bd691
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-13 21:01:55 +0530

    roomid

--- server/models/userTwo.js
+++ server/models/userTwo.js
@@ -3,18 +3,13 @@ const mongoose  = require("mongoose")
 const {Schema} = mongoose
 
 const userDetails = new Schema({
-    phone:{
-        type:Number
-    },
-    address:{
-        type:String
-    },
-    college:{
-        type:String
-    },
+    
     main:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"UserProfile"
+    },
+    room_id:{
+        type:String
     }
 })
 module.exports = mongoose.model("userDetails",userDetails)
