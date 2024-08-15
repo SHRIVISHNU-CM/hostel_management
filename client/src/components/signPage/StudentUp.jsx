41892e42951500bf6db6513c5d13f8dff7bd1a7d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-15 12:01:30 +0530

    client signup

--- client/src/components/signPage/StudentUp.jsx
+++ client/src/components/signPage/StudentUp.jsx
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FaUser } from "react-icons/fa6";
 import { MdPassword } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { userContext } from "../../Context/UserContext";
+import axios from "axios";
 
 function StudentUp() {
-    const[name,SetName] = useState("")
-    const[password,SetPassword] = useState("")
+    const { userName, setUserName, userPassword, setUserPassword } = useContext(userContext)
+    const HandleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/api/signup', {
+            name: userName,
+            password: userPassword
+        })
+            .then((res) => {
+                console.log(res)
+
+            })
+            .catch((e) => {
+                console.log(e)
+
+            })
+    }
     return (
         <>
             <div className="flex justify-center">
-                <form className="border px-4 py-8">
+                <form className="border px-4 py-8" onSubmit={HandleSubmit}>
                     <label className="input input-bordered flex items-center my-2">
-                        <FaUser className="text-slate-600"/>
-                        <input 
-                        value={name}
-                        onChange={e=>SetName(e.target.value)}
-                        type="text" placeholder="Enter username" />
+                        <FaUser className="text-slate-600" />
+                        <input
+                            value={userName}
+                            onChange={e => setUserName(e.target.value)}
+                            type="text" placeholder="Enter username" />
 
                     </label>
                     <label className="input input-bordered flex items-center my-2">
-                        <MdPassword className="text-slate-600"/>
-                        <input 
-                        value={password}
-                        onChange={e=>SetPassword(e.target.value)}
-                        type="text" placeholder="Enter Password" />
+                        <MdPassword className="text-slate-600" />
+                        <input
+                            value={userPassword}
+                            onChange={e => setUserPassword(e.target.value)}
+                            type="text" placeholder="Enter Password" />
 
                     </label>
                     <button type="submit" className="btn btn-secondary">Login</button>
