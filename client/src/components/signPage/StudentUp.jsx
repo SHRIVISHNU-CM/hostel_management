cff1dc6e81f99489a3c7a0d88ce0c72beb3bbcbe
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-19 20:35:11 +0530

    college,phone,address

--- client/src/components/signPage/StudentUp.jsx
+++ client/src/components/signPage/StudentUp.jsx
@@ -7,11 +7,17 @@ import axios from "axios";
 
 function StudentUp() {
     const { userName, setUserName, userPassword, setUserPassword } = useContext(userContext)
+    const [phone,setPhone] = useState("")
+    const [address,SetAddress] = useState("")
+    const [college,setCollege] = useState("")
     const HandleSubmit = (e) => {
         e.preventDefault()
         axios.post('http://localhost:3001/api/signup', {
             name: userName,
-            password: userPassword
+            password: userPassword,
+            college:college,
+            phone:phone,
+            address:address
         })
             .then((res) => {
                 console.log(res)
@@ -42,7 +48,32 @@ function StudentUp() {
                             type="text" placeholder="Enter Password" />
 
                     </label>
-                    <button type="submit" className="btn btn-secondary">Login</button>
+                    <label className="input input-bordered flex items-center my-2">
+                        <FaUser className="text-slate-600" />
+                        <input
+                            value={phone}
+                            onChange={e => setPhone(e.target.value)}
+                            type="text" placeholder="Enter Phone" />
+
+                    </label>
+                    <label className="input input-bordered flex items-center my-2">
+                        <FaUser className="text-slate-600" />
+                        <input
+                            value={college}
+                            onChange={e => setCollege(e.target.value)}
+                            type="text" placeholder="Enter College" />
+
+                    </label>
+                    <label className="input input-bordered flex items-center my-2">
+                        <FaUser className="text-slate-600" />
+                        <input
+                            value={address}
+                            onChange={e => SetAddress(e.target.value)}
+                            type="text" placeholder="Enter address" />
+
+                    </label>
+                    
+                    <button type="submit" className="btn btn-secondary">Sign up</button>
                     <p>Already have an Student Account? <Link to="/register/student" className="btn btn-link">Login</Link></p>
                 </form>
             </div>
