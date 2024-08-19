0c2bf72e2919891696bae957ca9247b32b420db3
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-19 13:07:36 +0530

    user,adminprofile,addpost , contact

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { userContext } from "../Context/UserContext";
 
 
 function Navbar() {
-    const {isloggedIn} = useContext(userContext)
+    const { isloggedIn, AdminLogin, acc } = useContext(userContext)
     return (
         <>
             <div className="navbar bg-base-200 ">
@@ -19,10 +19,14 @@ function Navbar() {
                             tabIndex={0}
                             className="menu menu-lg dropdown-content bg-base-200 rounded-box z-10 mt-4 w-52 shadow"
                         >
-                            <Link to="/about" className="my-2">About</Link>
+                            {
+                                AdminLogin ? <Link to="/contact">ContactUs</Link> :
+                                    <Link to="/about">About</Link>
+                            }
                             <Link to="/contact" className="my-2">Contact Us</Link>
-                            {isloggedIn? <Link to="/userprofile" className="my-2">Profile</Link>:
-                            <Link to="/register" className="my-2">Register</Link>
+                            {isloggedIn ?
+                                <Link to="/userprofile" className="my-2">Profile</Link> :
+                                <Link to="/register" className="my-2">Register</Link>
                             }
                         </ul>
                     </div>
@@ -35,17 +39,43 @@ function Navbar() {
                 </div>
                 <div className="hidden lg:block lg:navbar-end">
                     <div className="flex justify-around">
-                        <NavLink to='/about'
-                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>About</NavLink>
-                        <NavLink to="/contact"
-                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Contact Us</NavLink>
-                        {isloggedIn ? <NavLink to="/userprofile"
-                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Profile</NavLink> :
+                        {AdminLogin ?
+                            <NavLink to="/adminContact"
+                                className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}
+                            >All contact</NavLink> :
+                            <NavLink to='/about'
+                                className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>About</NavLink>
+                        }
+                        {AdminLogin ?
+                            <NavLink to="/addPost"
+                                className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Add Post</NavLink> :
+                            <NavLink to="/contact"
+                                className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Contact Us</NavLink>
+                        }
+
+                        {AdminLogin ?
+                            <NavLink to="/adminprofile"
+                                className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Admin Profile</NavLink>:
+                                null
+                            
+                        }
+                        {AdminLogin?
+                            <NavLink to="/userRegister"
+                            className={({isActive})=>`${isActive? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}
+                            >User</NavLink>:null
+
+                        }
+                        {acc ?
+                            null :
                             <NavLink to="/register"
                                 className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"} `}>Rooms</NavLink>
+
+                        }
+                        {isloggedIn ? <NavLink to="/userprofile"
+                            className={({ isActive }) => `${isActive ? "font-semibold btn btn-success" : "font-semibold btn btn-ghost"}`}>Profile</NavLink> :
+                            null
                         }
                     </div>
-
                 </div>
             </div>
         </>
