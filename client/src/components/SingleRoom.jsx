00a49d503266ffc4f7075753305e40ab858b9ca9
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-19 13:03:20 +0530

    price , location,country

--- client/src/components/SingleRoom.jsx
+++ client/src/components/SingleRoom.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import axios from "axios"
+import { userContext } from '../Context/UserContext'
 function SingleRoom() {
   const [room, SetRoom] = useState([])
   const { id } = useParams()
+  const {userID}  = useContext(userContext)
+  const [err , Seterr] = useState("")
   useEffect(() => {
     axios.get(`http://localhost:3001/admin/room/${id}`)
       .then((res) => {
@@ -12,6 +15,21 @@ function SingleRoom() {
       })
 
   }, [])
+  //function for bookNow 
+  const HandleBookNow = () => {
+    
+      axios.post(`http://localhost:3001/api/details`,{
+        main:userID,
+        room_id:id
+      })
+      .then((res)=>{
+        console.log(res)
+      })
+      .catch((e)=>{
+        Seterr(e.response.data)
+        console.log(e.response.data)
+      })
+  }
   return (
     <>
       <div className='card w-[600px] bg-base-200 shadow-xl'>
@@ -21,9 +39,14 @@ function SingleRoom() {
         <div className='card-body'>
           <h1 className='font-semibold text-xl'>Sharing:{room.sharing}</h1>
           <h1 className='font-semibold text-xl'>Availability: {room.availability}</h1>
+          <h1 className='font-semibold text-xl'>Price:{room.amount}</h1>
+          <h1 className='font-semibold text-xl'>Location: {room.location}</h1>
+          <h1 className='font-semibold text-xl'>Country: {room.country}</h1>
+
           <div>
-            <button className='btn btn-active'>Book Now</button>
+            <button className='btn btn-active' onClick={HandleBookNow}>Book Now</button>
           </div>
+          {err && <h1 className='font-light text-xl text-purple-400'>{err}</h1>}
         </div>
 
       </div>
