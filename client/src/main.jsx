601a4f1d6d1628c8d04ab651f5540b5517ec675a
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-19 11:11:25 +0530

    route for adminprofile,add post, admin update, userregister/admincontact

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,9 +14,14 @@ import Home from './components/Home.jsx'
 import AllRooms from './components/AllRooms.jsx'
 import SingleRoom from './components/SingleRoom.jsx'
 import Loading from './components/Loading.jsx'
+import AdminContact from "./components/dashboard/AdminContact.jsx"
+import UserRegister from "./components/dashboard/UserRegister.jsx"
 
 import { userContext } from './Context/UserContext.jsx'
 import UserProfile from './components/dashboard/UserProfile.jsx'
+import AddPost from './components/dashboard/AddPost.jsx'
+import AdminProfile from './components/dashboard/AdminProfile.jsx'
+import AdminUpdate from './components/dashboard/AdminUpdate.jsx'
 
 
 //lazy components
@@ -30,6 +35,11 @@ const router = createBrowserRouter(
         <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
         <Route path='userprofile' element={<UserProfile />} />
+        <Route path='adminprofile' element={<AdminProfile/>}/>
+        <Route path='adminContact' element={<AdminContact/>}/>
+        <Route path='addPost' element={<AddPost/>}/>
+        <Route path='adminupdate/:id' element={<AdminUpdate/>}/>
+        <Route path='userRegister' element={<UserRegister/>}/>
         <Route path='register' element={<Register />} >
           <Route index element={<Navigate to="admin" replace />} />
           <Route path='admin' element={<Admin />} />
@@ -55,6 +65,11 @@ function Root() {
   const [userPassword, setUserPassword] = useState("")
   const [userID, SetuserID] = useState('')
   const [isloggedIn, setIsloggedIn] = useState(false)
+  const [AdminName,SetAdminName] = useState("")
+  const [AdminID, SetAdminID] = useState("")
+  const [AdminLogin ,SetAdminLogin] = useState(false)
+  const [acc,setacc]  = useState(false)
+
 
   const login = (name, password) => {
     setUserName(name)
@@ -66,11 +81,32 @@ function Root() {
     setUserPassword("")
     setIsloggedIn(false)
   }
+  const AdminIsLoggin = (name) =>{
+    SetAdminLogin(true)
+    SetAdminName(name)
+  }
+  const AdminIsLogout  = () => {
+    SetAdminLogin(false)
+    SetAdminName("")
+  }
+  const Account = () => {
+    setacc(true)
+  }
 
   return (
     <React.StrictMode>
       <userContext.Provider value={{
-        userName, setUserName, userPassword, setUserPassword, login, logut, isloggedIn,userID , SetuserID
+        userName, 
+        setUserName, 
+        userPassword, 
+        setUserPassword, 
+        login, 
+        logut, 
+        isloggedIn,
+        userID , 
+        SetuserID,
+        AdminName,
+        SetAdminName,AdminID, SetAdminID,AdminLogin ,SetAdminLogin,AdminIsLoggin,AdminIsLogout,setacc,acc,Account
       }}>
         <RouterProvider router={router} />
       </userContext.Provider>
