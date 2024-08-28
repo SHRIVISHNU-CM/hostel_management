5a484b55053532ce958a3af8fcf390546cae08c6
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-28 22:22:39 +0530

    pre save jwttoken

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const JWT = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 const { Schema } = mongoose
 
@@ -30,4 +32,21 @@ const User = new Schema({
         ref: "userDetails"
     }]
 }, { timestamps: true })
+
+User.pre("save", async function (next) {
+    //if password is not modified then do not hash it
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    return next()
+})
+User.methods = {
+    jwtToken(){
+        return JWT.sign(
+            {id:this._id, name: this.name},
+            process.env.SECRET,
+            {expiresIn:"1h"}
+        )
+    }
+}
+
 module.exports = mongoose.model("UserProfile", User)
