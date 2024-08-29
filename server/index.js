f1d240e41c99f2634ec86e499581dacc11711ab5
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-29 11:48:48 +0530

    commenting

--- server/index.js
+++ server/index.js
@@ -6,24 +6,27 @@ const adminRoutes = require("./routes/adminRoutes")
 const DB = require('./models/connDB')
 const cloudinary = require("cloudinary").v2
 const cors = require("cors")
+const cookieparser = require("cookie-parser")
 
-DB()
+DB() // database connection established
+//configuration cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
-const port = 3001
+const port = 3001 // define port
 
 
 
-app.use(express.json())
+app.use(express.json()) //parse incoming json request
+app.use(cookieparser()) // parse incomming cookies
 app.use(cors(
     {
         origin:"http://localhost:5173",
         credentials:true
     }
-))
-app.use("/api", routes)
-app.use("/admin", adminRoutes)
-app.listen(port, () => console.log(`Server is running at ${port}`))
+)) // establish connection between frontend using cross origin resource sharing
+app.use("/api", routes) // routes for users
+app.use("/admin", adminRoutes) // routes for admin 
+app.listen(port, () => console.log(`Server is running at ${port}`)) // running server at port
