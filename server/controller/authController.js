71fd80cbbd9a03359d93f601a95809cfed377f4e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-29 13:19:01 +0530

    updating checkroom

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -8,7 +8,7 @@ const signup = async (req, res) => {
     try {
         if (!name || !password || !phone || !address || !college) {
             return res.status(400).json({
-                "message": "Enter your Name & Password"
+                "message": "Enter your all credentials"
             })
         }
         const OneUser = await user.findOne({ name: name })
@@ -41,22 +41,22 @@ const signin = async (req, res) => {
                 "message": "Provide correct Username & Password"
             })
         }
-        const result = await user.findOne({ name: name}).select("+password")
+        const result = await user.findOne({ name: name }).select("+password")
 
-        if (result && (await bcrypt.compare(password,result.password))) {
-            
+        if (result && (await bcrypt.compare(password, result.password))) {
+
             console.log(result)
             const token = result.jwtToken()
             result.password = undefined
             const cookieOptions = {
-                maxAge : 1*60*60*1000,
-                httpOnly:true
+                maxAge: 1 * 60 * 60 * 1000,
+                httpOnly: true
             }
-            
-            return res.cookie('token',token,cookieOptions).status(200).json({
+
+            return res.cookie('token', token, cookieOptions).status(200).json({
                 "message": result,
                 "info": "Successfully login",
-                "token":token
+                "token": token
             })
         } else {
             return res.status(401).json({
@@ -71,6 +71,25 @@ const signin = async (req, res) => {
         })
     }
 }
+//logout
+const logout = async (req, res) => {
+    try {
+        const cookieOptions = {
+            expires: new Date(),
+            httpOnly: true
+        }
+        return res.status(200).cookie('token', null, cookieOptions).json({
+            "message": "logout"
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            "message": "error"
+        })
+    }
+
+}
 
 //user's profile
 const userDetails = async (req, res) => {
@@ -78,29 +97,31 @@ const userDetails = async (req, res) => {
         const postInfo = req.body
         const newInfo = new userInfo(postInfo)
         const userData = await user.findById(postInfo.main)
-
+        const room_id = req.body.room_id
 
         if (!userData) {
             return res.status(400).json({
                 "message": "User Not Found"
             })
         }
-        const CheckRoom = await userInfo.findOne({room_id:req.body.room_id})
-        if (CheckRoom) {
+        console.log("room_id", room_id)
+        const CheckRoom = await userInfo.findOne({ room_id: room_id })
+        console.log(CheckRoom)
+        if (!CheckRoom) {
+            const info = await newInfo.save()
+
+            if (!userData.rooms) {
+                userData.rooms = [];
+            }
+
+            userData.rooms.push(info._id)
+            await userData.save()
+
+            res.status(200).json(info)
+        } else {
             return res.status(400).json("Already Registered. Thank You!")
+
         }
-        
-
-        const info = await newInfo.save()
-
-        if (!userData.rooms) {
-            userData.rooms = [];
-        }
-
-        userData.rooms.push(info._id)
-        await userData.save()
-
-        res.status(200).json(info)
     } catch (error) {
         console.log(error.message)
         return res.status(400).json("Bad request")
@@ -158,7 +179,7 @@ const deleteDetails = async (req, res) => {
 
 const updateDetails = async (req, res) => {
     try {
-        const {alloted } = req.body
+        const { alloted } = req.body
         const room = await userInfo.findByIdAndUpdate({ _id: req.params.id }, { alloted }, { new: true })
 
         console.log(room)
@@ -175,8 +196,12 @@ const updateDetails = async (req, res) => {
 //profile Update
 const profileUpdate = async (req, res) => {
     try {
-        const { name, password } = req.body
-        const response = await user.findByIdAndUpdate({ _id: req.params.id }, { name, password }, { new: true })
+        const { name, password, phone, address, college } = req.body
+        const updated = { name, phone, address, college }
+        if (password) {
+            updated.password = await bcrypt.hash(password, 10)
+        }
+        const response = await user.findByIdAndUpdate(req.params.id, updated, { new: true })
 
         console.log(response)
         return res.status(200).json({
@@ -194,7 +219,8 @@ const profileUpdate = async (req, res) => {
 const OneUser = async (req, res) => {
     try {
         const id = req.params.id
-        const response = await user.findById({ _id: id }).populate("rooms")
+        // const response = await user.findById({ _id: id }).populate("rooms").select("-password") // remove password field
+        const response = await user.findById({ _id: id }).populate("rooms") // remove password field
 
         console.log(response)
 
@@ -217,5 +243,6 @@ module.exports = {
     deleteDetails,
     updateDetails,
     profileUpdate,
-    OneUser
+    OneUser,
+    logout
 }
