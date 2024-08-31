b2cc21bf3b2d73713d3fa5011e8c03479b3cd676
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-31 19:17:02 +0530

    suspense

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,60 +3,92 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
-import About from './components/About.jsx'
-import Contact from './components/Contact.jsx'
 import Register from './components/Register.jsx'
-import Admin from './components/signPage/Admin.jsx'
-import Signin from './components/signPage/Student.jsx'
-import AdminsignUp from './components/signPage/AdminsignUp.jsx'
-import StudentUp from './components/signPage/StudentUp.jsx'
 import Home from './components/Home.jsx'
-import AllRooms from './components/AllRooms.jsx'
-import SingleRoom from './components/SingleRoom.jsx'
 import Loading from './components/Loading.jsx'
-import AdminContact from "./components/dashboard/AdminContact.jsx"
-import UserRegister from "./components/dashboard/UserRegister.jsx"
 
 import { userContext } from './Context/UserContext.jsx'
-import UserProfile from './components/dashboard/UserProfile.jsx'
-import AddPost from './components/dashboard/AddPost.jsx'
-import AdminProfile from './components/dashboard/AdminProfile.jsx'
-import AdminUpdate from './components/dashboard/AdminUpdate.jsx'
-import UserDetails from './components/dashboard/UserDetails.jsx'
-import UserEdit from './components/dashboard/UserEdit.jsx'
 
 
 //lazy components
 
 const LazyAllRooms = React.lazy(() => import("./components/AllRooms.jsx"))
+const SingleRoom = React.lazy(() => import('./components/SingleRoom.jsx'))
+const About = React.lazy(() => import('./components/About.jsx'))
+const Contact = React.lazy(() => import('./components/Contact.jsx'))
+const UserProfile = React.lazy(() => import('./components/dashboard/UserProfile.jsx'))
+const AdminProfile = React.lazy(() => import('./components/dashboard/AdminProfile.jsx'))
+const AdminContact = React.lazy(() => import("./components/dashboard/AdminContact.jsx"))
+const AddPost = React.lazy(() => import('./components/dashboard/AddPost.jsx'))
+const AdminUpdate = React.lazy(() => import('./components/dashboard/AdminUpdate.jsx'))
+const UserRegister = React.lazy(() => import("./components/dashboard/UserRegister.jsx"))
+const UserDetails = React.lazy(() => import('./components/dashboard/UserDetails.jsx'))
+const UserEdit = React.lazy(()=>import('./components/dashboard/UserEdit.jsx'))
+const Admin = React.lazy(() => import('./components/signPage/Admin.jsx'))
+const Signin = React.lazy(() => import('./components/signPage/Student.jsx'))
+const AdminsignUp = React.lazy(() => import('./components/signPage/AdminsignUp.jsx'))
+const StudentUp = React.lazy(() => import('./components/signPage/StudentUp.jsx'))
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path='/' element={<App />}>
         <Route path='' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='userprofile' element={<UserProfile />} />
-        <Route path='adminprofile' element={<AdminProfile/>}/>
-        <Route path='adminContact' element={<AdminContact/>}/>
-        <Route path='addPost' element={<AddPost/>}/>
-        <Route path='adminupdate/:id' element={<AdminUpdate/>}/>
-        <Route path='userRegister' element={<UserRegister/>}/>
-        <Route path='userdetails/:id' element={<UserDetails/>}/>
-        <Route path='useredit/:id' element={<UserEdit/>}/>
+        <Route path='about' element={
+          <Suspense fallback={<Loading />}>
+            <About />
+          </Suspense>} />
+        <Route path='contact' element={
+          <Suspense fallback={<Loading />}>
+            <Contact />
+          </Suspense>} />
+        <Route path='userprofile' element={<Suspense fallback={<Loading />}>
+          <UserProfile />
+        </Suspense>} />
+        <Route path='adminprofile' element={<Suspense fallback={<Loading />}>
+          <AdminProfile />
+        </Suspense>} />
+        <Route path='adminContact' element={<Suspense fallback={<Loading />}>
+          <AdminContact />
+        </Suspense>} />
+        <Route path='addPost' element={<Suspense fallback={<Loading />}>
+          <AddPost />
+        </Suspense>} />
+        <Route path='adminupdate/:id' element={<Suspense fallback={<Loading />}>
+          <AdminUpdate />
+        </Suspense>} />
+        <Route path='userRegister' element={<Suspense fallback={<Loading />}>
+          <UserRegister />
+        </Suspense>} />
+        <Route path='userdetails/:id' element={<Suspense fallback={<Loading />}>
+          <UserDetails />
+        </Suspense>} />
+        <Route path='useredit/:id' element={<Suspense fallback={<Loading/>}>
+          <UserEdit />
+        </Suspense>} />
         <Route path='register' element={<Register />} >
           <Route index element={<Navigate to="admin" replace />} />
-          <Route path='admin' element={<Admin />} />
-          <Route path='student' element={<Signin />} />
-          <Route path='AdminSignup' element={<AdminsignUp />} />
-          <Route path='studentUp' element={<StudentUp />} />
+          <Route path='admin' element={<Suspense fallback={<Loading/>}>
+            <Admin />
+          </Suspense>} />
+          <Route path='student' element={<Suspense fallback={<Loading/>}>
+            <Signin />
+          </Suspense>} />
+          <Route path='AdminSignup' element={<Suspense>
+            <AdminsignUp />
+          </Suspense>} />
+          <Route path='studentUp' element={<Suspense>
+            <StudentUp />
+          </Suspense>} />
         </Route>
         <Route path='allrooms' element={
           <Suspense fallback={<Loading />}>
             <LazyAllRooms />
           </Suspense>
         } />
-        <Route path='room/:id' element={<SingleRoom />} />
+        <Route path='room/:id' element={
+          <Suspense fallback={<Loading />}>
+            <SingleRoom />
+          </Suspense>} />
       </Route>
     </Route>
   )
@@ -69,10 +101,10 @@ function Root() {
   const [userPassword, setUserPassword] = useState("")
   const [userID, SetuserID] = useState('')
   const [isloggedIn, setIsloggedIn] = useState(false)
-  const [AdminName,SetAdminName] = useState("")
+  const [AdminName, SetAdminName] = useState("")
   const [AdminID, SetAdminID] = useState("")
-  const [AdminLogin ,SetAdminLogin] = useState(false)
-  const [acc,setacc]  = useState(false)
+  const [AdminLogin, SetAdminLogin] = useState(false)
+  const [acc, setacc] = useState(false)
 
 
 
@@ -87,11 +119,11 @@ function Root() {
     setIsloggedIn(false)
     setacc(false)
   }
-  const AdminIsLoggin = (name) =>{
+  const AdminIsLoggin = (name) => {
     SetAdminLogin(true)
     SetAdminName(name)
   }
-  const AdminIsLogout  = () => {
+  const AdminIsLogout = () => {
     SetAdminLogin(false)
     setacc(false)
     SetAdminName("")
@@ -103,17 +135,17 @@ function Root() {
   return (
     <React.StrictMode>
       <userContext.Provider value={{
-        userName, 
-        setUserName, 
-        userPassword, 
-        setUserPassword, 
-        login, 
-        logut, 
+        userName,
+        setUserName,
+        userPassword,
+        setUserPassword,
+        login,
+        logut,
         isloggedIn,
-        userID , 
+        userID,
         SetuserID,
         AdminName,
-        SetAdminName,AdminID, SetAdminID,AdminLogin ,SetAdminLogin,AdminIsLoggin,AdminIsLogout,setacc,acc,Account
+        SetAdminName, AdminID, SetAdminID, AdminLogin, SetAdminLogin, AdminIsLoggin, AdminIsLogout, setacc, acc, Account
       }}>
         <RouterProvider router={router} />
       </userContext.Provider>
