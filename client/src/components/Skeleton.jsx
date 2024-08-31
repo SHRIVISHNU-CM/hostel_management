03e71d8e188cfd0edd7ec108b275481a48585401
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-08-31 19:13:33 +0530

    skelton

---
+++ client/src/components/Skeleton.jsx
@@ -0,0 +1,14 @@
+
+function Skeleton(){
+    return(
+        <>
+            <div className="flex w-44 flex-col gap-9 items-center">
+                <div className="skeleton w-full h-8"></div>
+                <div className="skeleton h-8 w-28"></div>
+                <div className="skeleton h-8 w-28"></div>
+            </div>
+
+        </>
+    )
+}
+export default Skeleton
